c4181b1048371285e79d076ea28c8938936c1444
mariangel <hernandezmariangel006@gmail.com>
2026-06-08 17:05:36 -0300

    atualizou

--- 08-06-26/server.js
+++ 08-06-26/server.js
@@ -20,3 +20,60 @@ let chats = [
     ]
   }
 ];
+
+
+// ROTAS DE API //
+
+// lista de todos os chats //
+app.get('/api/chats', (req, res) => {
+    const chat = chats.find(c => c.id === req.params.orderId);
+    res.json(chat);
+});
+
+
+// buscar detalhes de um chat específico //
+app.get('/api/chats/:orderId', (req, res) => {
+    const chat = chats.find(c => c.id === req.params.orderId);
+    if (!chat) {
+        return res.status(404).json({ error: 'Chat não encontrado' });
+    }
+});
+
+// enviar uma messagem nova //
+app.post('/api/chats/:orderId/messages', (req, res) => {
+    const {orderId} = req.params;
+    const {sender, text} = req.body;
+    if (!sender || !text) {
+        return res.status(404).json({ error: 'Os campos sender e text sâo encontrados' });
+    }
+    const chat = chats.find(c => c.id === orderId);
+    if (!chat) {
+        return res.status(404).json({ error: 'Chat não encontrado' });
+    }
+    
+});
+
+
+  // gerando um timestamp simples
+  const now = new Date();
+  const timestamp = `${String(now.getHours()).padStart(2, '0')}:${String(now.getMinutes()).padStart(2, '0')}`;
+
+  const newMessage = {
+    id: (chat.messages?.length || 0) + 1,
+    sender,
+    text,
+    timestamp
+  };
+
+  chat.messages = chat.messages || [];
+  chat.messages.push(newMessage);
+
+  return res.status(201).json(newMessage);
+
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta em: http://localhost: ${PORT}`);
+});
